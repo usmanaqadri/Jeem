4994880fc5add581cb5795f1a2074729490e0fb3
usmanaqadri <usmanaqadri@gmail.com>
2024-06-21 19:50:29 -0400

    added github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://usmanaqadri.github.io",
+  base: "/Jeem",
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
 });
