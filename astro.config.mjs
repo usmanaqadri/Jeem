1b8afe763cfe1c03b15e066c619959c59f324de8
usmanaqadri <usmanaqadri@gmail.com>
2024-06-21 19:56:22 -0400

    removed stuff from astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import { defineConfig } from "astro/config";
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://usmanaqadri.github.io",
   base: "/Jeem",
-  output: "server",
-  adapter: vercel(),
 });
